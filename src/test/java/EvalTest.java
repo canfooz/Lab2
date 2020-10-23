4bc91bd44057067467732b7f37afd2dfca04b56e
canfooz <canfooz2000@gmail.com>
2020-10-23 21:52:20 +0300

    added tests

--- src/test/java/EvalTest.java
+++ src/test/java/EvalTest.java
@@ -1,30 +1,61 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class EvalTest {
 
-class EvalTest {
-
-    @Test
-    void nextChar() {
+    @BeforeEach
+    public void RefreshString() {
+        Eval.setStr("");
     }
 
     @Test
-    void eat() {
+    public void EvalParsesDefaultExpression() {
+        String expression = "2+3*5";
+        double expectedValue = 17;
+        Eval.setStr(expression);
+        Assertions.assertEquals(expectedValue, Eval.parse());
     }
 
     @Test
-    void parse() {
+    public void EvalParsesEquationWithSpaces() {
+        String expression = "2+ 3 *5";
+        double expectedValue = 17;
+        Eval.setStr(expression);
+        Assertions.assertEquals(expectedValue, Eval.parse());
     }
 
     @Test
-    void parseExpression() {
+    public void EvalParsesEquationWithParentheses() {
+        String expression = "2+3*(5-2*2)";
+        double expectedValue = 5;
+        Eval.setStr(expression);
+        Assertions.assertEquals(expectedValue, Eval.parse());
+
     }
 
     @Test
-    void parseTerm() {
+    public void EvalParsesEquationWithFunctions() {
+        String expression = "sin(0)+cos(0)";
+        double expectedValue = 1;
+        Eval.setStr(expression);
+        Assertions.assertEquals(expectedValue, Eval.parse());
+
     }
 
     @Test
-    void parseFactor() {
+    public void EvalThrowsExceptionForUnknownFunction(){
+        String expression = "2+tanxhz(4)";
+        Eval.setStr(expression);
+        Assertions.assertThrows(RuntimeException.class, Eval::parse);
+
     }
+
+    @Test
+    public void EvalThrowsExceptionForWrongExpression ( ) {
+        String expression = "2+4abc";
+        Eval.setStr(expression);
+        Assertions.assertThrows(RuntimeException.class, Eval::parse);
+    }
+
 }
